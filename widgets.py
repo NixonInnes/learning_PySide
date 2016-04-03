e45d0bf064c85007446c732a1d0ac7541faef168
nixoninnes <nixoninnes@gmail.com>
2016-04-03 14:11:33 +0100

    working through widgets

--- widgets.py
+++ widgets.py
@@ -176,7 +176,29 @@ from PySide import QtGui, QtCore
 #         self.lbl.setText(date.toString())
 
 
-# QtGui.QPixmap
+# # QtGui.QPixmap
+# class Example(QtGui.QWidget):
+#
+#     def __init__(self):
+#         super(Example, self).__init__()
+#         self.initUI()
+#
+#     def initUI(self):
+#         hbox = QtGui.QHBoxLayout(self)
+#         pixmap = QtGui.QPixmap('planet.png')
+#
+#         lbl = QtGui.QLabel(self)
+#         lbl.setPixmap(pixmap)
+#
+#         hbox.addWidget(lbl)
+#         self.setLayout(hbox)
+#
+#         self.setGeometry(300, 300, 280, 170)
+#         self.setWindowTitle('Planet')
+#         self.show()
+
+
+# QtGui.QLineEdit
 class Example(QtGui.QWidget):
 
     def __init__(self):
@@ -184,18 +206,21 @@ class Example(QtGui.QWidget):
         self.initUI()
 
     def initUI(self):
-        hbox = QtGui.QHBoxLayout(self)
-        pixmap = QtGui.QPixmap('redrock.png')
+        self.lbl = QtGui.QLabel(self)
+        self.lbl.move(60, 40)
 
-        lbl = QtGui.QLabel(self)
-        lbl.setPixmap(pixmap)
+        qle = QtGui.QLineEdit(self)
+        qle.move(60, 100)
 
-        hbox.addWidget(lbl)
-        self.setLayout(hbox)
+        qle.textChanged[str].connect(self.onChanged)
 
         self.setGeometry(300, 300, 280, 170)
-        self.setWindowTitle('Red Rock')
+        self.setWindowTitle('QtGui.QLineEdit')
         self.show()
+
+    def onChanged(self, text):
+        self.lbl.setText(text)
+        self.lbl.adjustSize()
 
 
 def main():
